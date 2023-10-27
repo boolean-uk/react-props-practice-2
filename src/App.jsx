507bd5638da900806c700fc0b8ea5e5ddcafdbb3
Nazar Tymiv <nazartumiv@gmail.com>
2023-10-27 12:36:27 +0100

    Moved state from title into title component :

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
-
 import Chat from "./components/Chat";
 import Title from "./components/Title";
 
 import "./App.css";
 
 export default function App() {
-    const [user, setUser] = useState("Nicolas");
-
     return (
         <div className="app">
-            <Title user={user} />
+            <Title />
             <Chat />
         </div>
     );
