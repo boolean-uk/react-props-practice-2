f288681d4af063336a72a39ad370321d889a2dfb
Faiza <faraz_ul_islam@yahoo.com>
2023-10-27 21:14:16 +0100

    props practice-2

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,29 @@
-import Message from './Message'
+//import Message from './Message'
 
-function Chat({ messages, addMessage }) {
+
+const intialMessages = [
+  { id: 1, text: 'Hello!', user: 'Nicolas' },
+  { id: 2, text: 'Hey!', user: 'Sergio' },
+  { id: 3, text: 'How are you feeling today?', user: 'Nicolas' },
+  { id: 4, text: 'Hot hot, you?', user: 'Sergio' },
+  { id: 5, text: 'Cool cool!', user: 'Nicolas' }
+]
+
+
+
+
+
+
+
+function Chat({  }) {
+
+  const [messages, setMessages] = useState(intialMessages)
+
+  const addMessage = text => {
+  
+    setMessages([...messages, { id: id++, text, user: 'Nicolas' }])
+  }
+
   return (
     <div className="chat">
       <ul>
