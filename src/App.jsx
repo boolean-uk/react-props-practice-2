376b0e14aad948e8425fa442204fd59c0ae9b3c5
Mahmoud Ali <mahmoodali2965.ma@gmail.com>
2024-02-23 11:28:50 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,7 @@ import Title from './components/Title'
 import './App.css'
 
 
-const initialMessages = [
-  { id: 1, text: 'Hello!', user: 'Nicolas' },
-  { id: 2, text: 'Hey!', user: 'Sergio' },
-  { id: 3, text: 'How are you feeling today?', user: 'Nicolas' },
-  { id: 4, text: 'Hot hot, you?', user: 'Sergio' },
-  { id: 5, text: 'Cool cool!', user: 'Nicolas' }
-]
+
 
 export default function App() {
   
